0ad43676c2289d17ce2eb4d06767f06e65f42f90
Len_zH <lenzhgx@gmail.com>
2025-07-05 22:03:32 +0800

    添加菜单

---
+++ themes/PinBlog/source/js/main.js
@@ -0,0 +1,52 @@
+const app = Vue.createApp({
+    mixin: Object.values(mixins),
+    data() {
+        return {
+            loading: true,
+            hiddenMenu: true,
+            showMenu: false,
+            menuColor: false,
+            scrollTop: 0,
+            renderers: [],
+        };
+    },
+    created() {
+        window.addEventListener("load", () => {
+            this.loading = false;
+        })
+    },
+    mounted() {
+        window.addEventListener("scroll", this.handleScroll, true);
+        this.render();
+    },
+    methods: {
+        render() {
+            for (let i of this.renderers) i();
+        },
+        handleScroll() {
+            let wrap = this.$refs.homePostsWarp();
+            let newScrollTop = document.documentElement.scrollTop;
+            if (this.scrollTop < newScrollTop) {
+                this.hiddenMenu = true;
+                this.showMenu = false;
+            } else {
+                this.hiddenMenu = false;
+            }
+            if (wrap) {
+                if (newScrollTop <= window.innerHeight - 100) {
+                    this.menuColor = true;
+                } else {
+                    this.menuColor = false;
+                }
+                if (newScrollTop <= 400) {
+                    wrap.style.top = "-"+newScrollTop/5+"px";
+                } else {
+                    wrap.style.top = "-80px";
+                }
+            }
+            this.scrollTop = newScrollTop;
+        },
+    },
+})
+
+app.mount("#layout");
